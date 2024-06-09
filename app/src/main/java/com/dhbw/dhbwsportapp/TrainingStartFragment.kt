ea450037fe741d94f664901c44527d935a1149bf
JonNoe <jon.noe.23@lehre.mosbach.dhbw.de>
2024-06-09 14:54:34 +0200

    Pause Timer improvement & Training Design

--- app/src/main/java/com/dhbw/dhbwsportapp/TrainingStartFragment.kt
+++ app/src/main/java/com/dhbw/dhbwsportapp/TrainingStartFragment.kt
@@ -6,22 +6,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ImageButton
+import android.widget.ProgressBar
 import android.widget.TextView
 
 class TrainingStartFragment : Fragment() {
 
     private lateinit var timerTextView: TextView
     private lateinit var currentExerciseTextView: TextView
+    private lateinit var exerciseCountTextView: TextView
     private lateinit var countDownTimer: CountDownTimer
     private lateinit var prevButton: ImageButton
     private var pauseButton: ImageButton? = null
     private lateinit var nextButton: ImageButton
+    private lateinit var progressBar: ProgressBar
     private var isPaused = false
     private var timeRemaining: Long = 40000
     private var currentExerciseIndex = 0
+    private var totalExercises = 0
     private lateinit var exerciseTitles : List<String>
+    private var isNavigating = false
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -29,19 +34,23 @@ class TrainingStartFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_training_start, container, false)
 
-
         timerTextView = view.findViewById(R.id.timerTextView)
         currentExerciseTextView = view.findViewById(R.id.currentExerciseTextView)
-        prevButton = view.findViewById((R.id.previous))
-        pauseButton = view.findViewById((R.id.pause))
-        nextButton = view.findViewById((R.id.next))
+        exerciseCountTextView = view.findViewById(R.id.exerciseCountTextView)
+        prevButton = view.findViewById(R.id.previous)
+        pauseButton = view.findViewById(R.id.pause)
+        nextButton = view.findViewById(R.id.next)
+        progressBar = view.findViewById<ProgressBar>(R.id.progressBar)
 
         exerciseTitles = arguments?.getStringArrayList("exerciseTitles") ?: listOf()
         currentExerciseIndex = arguments?.getInt("currentExerciseIndex") ?: 0
+        totalExercises = exerciseTitles.size
+
+        updateExerciseCountText()
 
         if (exerciseTitles.isNotEmpty()){
             currentExerciseTextView.text = exerciseTitles[currentExerciseIndex]
-        }else{
+        } else {
             currentExerciseTextView.text = "No exercises"
         }
 
@@ -49,7 +58,10 @@ class TrainingStartFragment : Fragment() {
         startCountDownTimer(timeRemaining)
 
         nextButton.setOnClickListener{
-            navigateToPauseFragment()
+            if (!isNavigating) {
+                isNavigating = true
+                navigateToPauseFragment()
+            }
         }
 
         pauseButton?.setOnClickListener{
@@ -64,6 +76,11 @@ class TrainingStartFragment : Fragment() {
             }
         }
 
+        prevButton = view.findViewById(R.id.previous)
+        prevButton.setOnClickListener {
+            navigateToPreviousFragment()
+        }
+
         return view
     }
 
@@ -71,13 +88,16 @@ class TrainingStartFragment : Fragment() {
         countDownTimer = object : CountDownTimer(timeInMillis,1000) {
             override fun onTick(millisUntilFinished: Long) {
                 timeRemaining = millisUntilFinished
-                val minutes = millisUntilFinished/600000
+                val minutes = millisUntilFinished / 60000
                 val seconds = (millisUntilFinished % 60000) / 1000
                 timerTextView.text = String.format("%02d:%02d", minutes, seconds)
             }
             override fun onFinish() {
                 timerTextView.text = "00:00"
-                navigateToPauseFragment()
+                if (!isNavigating) {
+                    isNavigating = true
+                    navigateToPauseFragment()
+                }
             }
         }
         countDownTimer.start()
@@ -88,42 +108,55 @@ class TrainingStartFragment : Fragment() {
         countDownTimer.cancel()
     }
 
-    fun setNextExercise(){
-        currentExerciseIndex++
-        if (currentExerciseIndex < exerciseTitles.size){
-            currentExerciseTextView.text = exerciseTitles[currentExerciseIndex]
-            timeRemaining = 40000
-            startCountDownTimer(timeRemaining)
-        }else{
-            navigateToResultFragment()
-        }
-    }
-
     private fun navigateToPauseFragment(){
-        if (isAdded) {
-            val trainingBreakFragment = TrainingBreakFragment()
 
-            // Pass current exercise index to the break fragment
+        if (currentExerciseIndex < exerciseTitles.size - 1) {
+            val trainingBreakFragment = TrainingBreakFragment()
             val args = Bundle()
             args.putStringArrayList("exerciseTitles", ArrayList(exerciseTitles))
             args.putInt("currentExerciseIndex", currentExerciseIndex)
             trainingBreakFragment.arguments = args
 
+            if (isAdded && !requireActivity().supportFragmentManager.isStateSaved) {
+                val transaction = requireActivity().supportFragmentManager.beginTransaction()
+                transaction.replace(R.id.fragment_container_trainings, trainingBreakFragment)
+                transaction.addToBackStack(null)
+                transaction.commit()
+            }
+        } else {
+            navigateToResultFragment()
+        }
+    }
+
+    private fun navigateToResultFragment(){
+        if (isAdded) {
+            val resultFragment = ResultFragment()
             val transaction = requireActivity().supportFragmentManager.beginTransaction()
-            transaction.replace(R.id.fragment_container_trainings, trainingBreakFragment)
+            transaction.replace(R.id.fragment_container_trainings, resultFragment)
             transaction.addToBackStack(null)
             transaction.commit()
         }
     }
 
-    private fun navigateToResultFragment(){
-        val trainingBreakFragment = ResultFragment()
-        val transaction = requireActivity().supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.fragment_container_trainings, trainingBreakFragment)
-        transaction.addToBackStack(null)
-        transaction.commit()
+    private fun navigateToPreviousFragment(){
+        if (currentExerciseIndex > 0){
+            currentExerciseIndex--
+            timeRemaining = 40000
+            currentExerciseTextView.text = exerciseTitles[currentExerciseIndex]
+            startCountDownTimer(timeRemaining)
+        }
     }
 
+    private fun updateExerciseCountText() {
+        if (exerciseTitles.isNotEmpty()){
+            exerciseCountTextView.text = "${currentExerciseIndex + 1}/$totalExercises"
+            val progress = ((currentExerciseIndex+1)/exerciseTitles.size.toFloat())*100
+            progressBar.progress = progress.toInt()
+        }
+        else {
+            exerciseCountTextView.text = ""
+            progressBar.progress = 0
+        }
 
+    }
 }
-
